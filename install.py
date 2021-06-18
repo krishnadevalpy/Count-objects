71249589f577c758b29d451c3f1d67ce7d3f3b1d
Krishna Dev R <84676110+krishnadevalpy@users.noreply.github.com>
2021-06-18 23:02:22 +0530

    Create install.py

---
+++ install.py
@@ -0,0 +1,5 @@
+#The fix is either you install the latest master branch of openCV 
+
+#You can install another version of OpenCV by the following snippet:
+
+!pip install opencv-contrib-python==3.4.13.47 --force-reinstall
